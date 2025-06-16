7f1ce1c1b7ed31070e560ca18d2fb6bc725cae00
Jefri Herdi Triyanto <jefripunza@gmail.com>
2025-06-16 09:58:44 +0700

    nice try 1

--- script/nest-pack.sh
+++ script/nest-pack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # description: fast init NestJS project with user module and authorization with JWT
-# example: curl -fsSL https://init.jefripunza.com/nest-pack.sh | bash -s -- my-project-name
+# example: curl -fsSL https://init.jefripunza.com/nest-pack.sh -o nest-pack.sh && bash nest-pack.sh my-project-name
 
 set -e # Exit immediately if a command exits with a non-zero status
 ## $1 could be empty, so we need to disable this check
@@ -10,7 +10,11 @@ set -o pipefail # Cause a pipeline to return the status of the last command that
 DATE=$(date +"%Y%m%d-%H%M%S")
 CURRENT_USER=$USER
 
-# check apakah di command ada "nest", kalau belum ada di install
+### ========================================================================== ###
+###                             CREATE PROJECT                                 ###
+### ========================================================================== ###
+
+### check apakah di command ada "nest", kalau belum ada di install
 if ! command -v nest &> /dev/null; then
     echo "nest not found, installing..."
     bun add -g @nestjs/cli
@@ -20,4 +24,34 @@ if [ -z "$1" ]; then
     echo "Please provide a project name"
     exit 1
 fi
-nest new $1
+project_name=$1
+nest new $project_name
+
+### ========================================================================== ###
+###                               INSTALLATION                                 ###
+### ========================================================================== ###
+
+# masuk ke folder project
+pushd $project_name
+
+# buat variable array untuk module apa saja yang ingin ditambahkan custom
+module_list=("jsonwebtoken" "dotenv" "@nestjs/config")
+
+# loop array dan install module
+for module in "${module_list[@]}"; do
+    bun add $module
+done
+
+### ========================================================================== ###
+###                               GENERATION                                   ###
+### ========================================================================== ###
+
+# full package for User
+nest generate module user
+nest generate controller user
+nest generate service user
+
+# full package for auth
+nest generate module auth
+nest generate controller auth
+nest generate service auth
